3dff8364dca566e3e689be82a38e2a562f2398c9
thegodforce <theultimatekamen@gmail.com>
2019-05-07 16:19:58 -0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,63 @@
+var fs = require('fs'),
+	path = require('path'),
+	express = require('express'),
+	formidable = require('formidable');
+
+var app = express();
+var port = 3000;
+var output = path.resolve(__dirname, '../keygen_files');
+
+console.log('Output directory: ', output);
+
+app.use(express.static(__dirname + '/html'));
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+app.post('/upload', function(req, res) {
+	var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+     	var newpath = path.resolve(output, files.keywords.name);
+     	console.log('Temp file: ' + files.keywords.path);
+     	fs.rename(files.keywords.path, newpath, function (err) {
+        	if (err) {
+        		res.send('Cannot upload file');
+        	} else {
+        		readFile(newpath, res);
+        	}
+        });
+    });
+}); 
+
+app.listen(port, function(err) {
+	if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+function readFile(path, res) {
+	console.log('Open file:', path);
+
+	fs.readFile(path, 'utf8', function (err, data) {
+		if (err) {
+			console.err('Error while reading file.', err);
+	        res.send('File Error!');
+	    } else {
+		  var lines = data.split(/\r\n|\n|\r/);
+		  var output = [];
+		  lines.forEach(function(line) {
+		  	var col1 = line.split(',')[0];
+		  	lines.forEach(function(line) {
+		  		var col2 = line.split(',')[1];
+		  		if (col1 !== col2) {
+		  			output.push(col1 + ' ' + col2);
+		  		}
+		  	});
+		  });
+		  res.send('<a href="/">Upload more</a><br>' + output.join('<br>'));
+		}
+	});
+}
